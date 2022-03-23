5572e1edd66a1bd66d3f35e7baefc05fb7ae55f4
sasha <zooblin@gmail.com>
2022-03-23 09:11:14 +0200

    add dev account

--- dev_account/main.go
+++ dev_account/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/iam"
 	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -17,4 +18,20 @@ func main() {
 		ctx.Export("bucketName", bucket.ID())
 		return nil
 	})
+}
+
+func deployIAM(ctx *pulumi.Context) error {
+	_, roleErr := iam.NewRole(ctx, "developers", &iam.RoleArgs{
+		ManagedPolicyArns: pulumi.StringArray{
+			pulumi.String("arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"),
+		},
+		Tags: pulumi.StringMap{
+			"tag-key": pulumi.String("tag-value"),
+		},
+	})
+
+	if (roleErr != nil) {
+		return roleErr
+	}
+	return nil
 }
